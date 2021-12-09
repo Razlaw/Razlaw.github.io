b997d0d7a26b7a42ec187ec4ea8b229dea67ff6d
Razlaw <jan.razlaw@gmx.de>
2021-12-09 17:17:17 +0100

    Add icons to top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
@@ -6,6 +7,14 @@ export default function Topbar() {
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Link to home</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>124 56</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>mail adr</span>
+                    </div>
                 </div>
                 <div className="right">
 
