03c1f5766908e796331feb82e17b285762f0317e
Razlaw <jan.razlaw@gmx.de>
2021-12-09 17:15:12 +0100

    Add link to home to left top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,6 +5,7 @@ export default function Topbar() {
         <div className="topbar">
             <div className="wrapper">
                 <div className="left">
+                    <a href="#intro" className="logo">Link to home</a>
                 </div>
                 <div className="right">
 
