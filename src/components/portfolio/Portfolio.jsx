b7a4170bdcf8ffe5b083311b774baef24147dd85
Razlaw <jan.razlaw@gmx.de>
2021-12-17 16:37:42 +0100

    Add topic list to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,24 @@ import "./portfolio.scss";
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">
+                    Featured
+                </li>
+                <li>
+                    Web App
+                </li>
+                <li>
+                    Mobile App
+                </li>
+                <li>
+                    Design
+                </li>
+                <li>
+                    Branding
+                </li>
+            </ul>
         </div>
     );
 }
