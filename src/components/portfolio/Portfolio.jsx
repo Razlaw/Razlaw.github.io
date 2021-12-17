785a3810ba8ebd0532fe21d292c3a1c195489355
Razlaw <jan.razlaw@gmx.de>
2021-12-17 16:38:06 +0100

    Add first prototype item to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -21,6 +21,12 @@ export default function Portfolio() {
                     Branding
                 </li>
             </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900" alt=""/>
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     );
 }
