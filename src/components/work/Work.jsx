818091eca6c1541bfa7fdd831eb4635519274c15
Razlaw <jan.razlaw@gmx.de>
2021-12-20 23:24:30 +0100

    Use images from data for work examples

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -63,7 +63,7 @@ export default function Work() {
                                 </div>
                             </div>
                             <div className="right">
-                                <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930" alt="Test image"/>
+                                <img src={d.img} alt=""/>
                             </div>
                         </div>
                     </div>
