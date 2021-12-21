a33c7689c9da4c184d7369f550f6380c8f25382b
Razlaw <jan.razlaw@gmx.de>
2021-12-21 15:52:36 +0100

    Remove i-typed package

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,19 +1,6 @@
 import "./intro.scss";
-import {init} from "ityped";
-import {useEffect, useRef} from "react";
-
 
 export default function Intro() {
-    const textRef = useRef();
-
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: true,
-            delaySpeed: 60,
-            backDelay: 1500,
-            strings: ["String 1", "String 2", "String 3"]
-        });
-    }, []);
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -25,7 +12,6 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Name</h1>
-                    <h3>Job <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="Arrow pointing down"/>
