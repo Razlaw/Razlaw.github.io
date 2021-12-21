2d5a3bc3677270520404f86f6196055eb4e7ef4c
Razlaw <jan.razlaw@gmx.de>
2021-12-21 16:01:03 +0100

    Remove icons with mail address and phone number from top bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,21 +1,9 @@
 import "./topbar.scss";
-import {Person, Mail} from "@material-ui/icons";
 
 export default function Topbar({menuOpen, setMenuOpen}) {
     return (
         <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">Home</a>
-                    <div className="itemContainer">
-                        <Person className="icon"/>
-                        <span>124 56</span>
-                    </div>
-                    <div className="itemContainer">
-                        <Mail className="icon"/>
-                        <span>mail adr</span>
-                    </div>
-                </div>
                 <div className="right">
                     <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                         <span className="line1"></span>
