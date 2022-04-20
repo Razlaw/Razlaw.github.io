8c017baf4b10d6906400ff9b17826943433f38bc
Razlaw <jan.razlaw@gmx.de>
2022-04-20 15:41:53 +0200

    Remove sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import Sidebar from "./components/sidebar/Sidebar";
 import Intro from "./components/intro/Intro";
 import Work from "./components/work/Work";
 import "./app.scss"
-import {useState} from "react";
 
 function App() {
-    const [menuOpen, setMenuOpen] = useState(false)
     return (
         <div className="app">
-            <Sidebar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
             <div className="sections">
                 <Intro/>
                 <Work/>
