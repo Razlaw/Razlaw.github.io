42d25da35b2402cc10dcabed8b4eb0b42e614225
Razlaw <jan.razlaw@gmx.de>
2022-05-24 15:08:26 +0200

    Add new draft for projects section

--- src/components/project1/Project1.jsx
+++ src/components/project1/Project1.jsx
@@ -1,26 +1,29 @@
 import "./project1.scss";
 
+import React from "react";
+
+import cameramanImage from "../../assets/cameraman_downscaled.jpg";
+import lineForCameramanImage from "../../assets/line_for_cameraman.png";
+
 export default function Project1() {
     return (
-        <div className="project1">
-            <div className="leftContainer">
-                <div className="imgContainer">
-                    <img src="https://github.com/AIS-Bonn/rviz_cinematographer/blob/master/readme/output.gif?raw=true" alt="Portrait of Man"/>
-                </div>
+        <div className="cinematographerContainer">
+            <h1 className="projectsHeadline">Meine Projekte</h1>
+            <div className="imageContainer">
+                <img className="cameramanImage" src={cameramanImage} alt="Cameraman on a hill with mountains in the background"/>
+                <img className="lineForCameramanImage" src={lineForCameramanImage} alt="Lines indicating motion of camera"/>
             </div>
-            <div className="rightContainer">
-                <div className="textContainer">
-                    <h1 className="headlineBig" id="projectName">Rviz Cinematographer</h1>
-                    <p className="text" id="projectDescription">
-                        Open Source Paket um benutzerfreundlich komplexe Kamerafahrten in Rviz zu erstellen.
-                    </p>
-                    <a
-                        href="https://github.com/AIS-Bonn/rviz_cinematographer"
-                        target="_blank"
-                        rel="noreferrer">
-                        Link to repo
-                    </a>
-                </div>
+            <div className="textContainer">
+                <h1 className="headlineBig" id="projectName">RViz Cinematographer</h1>
+                <p className="text" id="projectDescription">
+                    Komplexe Kamerafahrten benutzerfreundlich in RViz erstellen
+                </p>
+                <a
+                    href="https://github.com/AIS-Bonn/rviz_cinematographer"
+                    target="_blank"
+                    rel="noreferrer">
+                    Code + icon external link
+                </a>
             </div>
         </div>
     );
--- src/components/project2/Project2.jsx
+++ src/components/project2/Project2.jsx
@@ -1,26 +1,34 @@
 import "./project2.scss";
 
+import React from "react";
+
+import dancersImage from "../../assets/dancers_downscaled.jpg";
+import linesForDancersImage from "../../assets/lines_for_dancers.png";
+
 export default function Project2() {
     return (
-        <div className="project2">
-            <div className="leftContainer">
-                <div className="imgContainer">
-                    <img src="https://github.com/AIS-Bonn/pose_graph_prediction/blob/master/docs/readme/pipeline_overview.png?raw=true" alt="Portrait of Man"/>
-                </div>
+        <div className="poseGraphPredictionContainer">
+            <h1 className="projectsHeadline">Meine Projekte</h1>
+            <div className="imageContainer">
+                <img className="dancersImage" src={dancersImage} alt="Three dancers in one motion"/>
+                <div className="upperLineCue" id="cueOne"/>
+                <div className="upperLineCue" id="cueTwo"/>
+                <div className="upperLineCue" id="cueThree"/>
+                <div className="upperLineCue" id="cueFour"/>
+                <div className="lowerLineCue"/>
+                <img className="linesForDancersImage" src={linesForDancersImage} alt="Lines indicating motion of dancers"/>
             </div>
-            <div className="rightContainer">
-                <div className="textContainer">
-                    <h1 className="headlineBig" id="projectName">Pose Graph Prediction</h1>
-                    <p className="text" id="projectDescription">
-                        Vorhersage von Bewegungen durch Graph Neuronale Netze.
-                    </p>
-                    <a
-                        href="https://github.com/AIS-Bonn/pose_graph_prediction"
-                        target="_blank"
-                        rel="noreferrer">
-                        Link to repo
-                    </a>
-                </div>
+            <div className="textContainer">
+                <h1 className="headlineBig" id="projectName">Pose Graph Prediction</h1>
+                <p className="text" id="projectDescription">
+                    Vorhersage von Bewegungen durch Graph Neuronale Netze.
+                </p>
+                <a
+                    href="https://github.com/AIS-Bonn/pose_graph_prediction"
+                    target="_blank"
+                    rel="noreferrer">
+                    Code + icon external link
+                </a>
             </div>
         </div>
     );
