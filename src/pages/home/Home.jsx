9181e561cc705af27cfd80809270dad954a7057a
Razlaw <jan.razlaw@gmx.de>
2022-05-24 16:10:38 +0200

    Add draft for international page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,12 +4,14 @@ import React from "react";
 
 import Intro from "../../pages/intro/Intro";
 import ProjectsContainer from "../../components/projectsContainer/ProjectsContainer";
+import International from "../../pages/international/International";
 
 export default function Home() {
     return (
         <div className="sections">
             <Intro/>
             <ProjectsContainer/>
+            <International/>
         </div>
     );
 }
---
+++ src/pages/international/International.jsx
@@ -0,0 +1,25 @@
+import "./international.scss";
+
+import {ReactComponent as NorthAmerica} from '../../assets/north_america_wo_borders.svg';
+import {ReactComponent as Eurasia} from '../../assets/eurasia_wo_borders.svg';
+
+export default function International() {
+    return (
+        <div className="internationalContainer">
+            <h1 className="internationalTitle">
+                International
+            </h1>
+            <div className="mapContainer">
+                <div className="imgContainer">
+                    <NorthAmerica className="northAmerica"/>
+                    <Eurasia className="eurasia"/>
+                </div>
+            </div>
+            <div className="textContainer">
+                <h1>
+                    Text
+                </h1>
+            </div>
+        </div>
+    );
+}
