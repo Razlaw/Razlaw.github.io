4fb6ef700a47981d8f1de5b7bf09221af6c8134e
Razlaw <jan.razlaw@gmx.de>
2022-05-25 20:48:33 +0200

    Add scroll snap compatible styling for international section

--- src/pages/international/International.jsx
+++ src/pages/international/International.jsx
@@ -5,20 +5,22 @@ import {ReactComponent as Eurasia} from '../../assets/eurasia_wo_borders.svg';
 
 export default function International() {
     return (
-        <div className="internationalContainer">
-            <h1 className="internationalTitle">
-                International
-            </h1>
-            <div className="mapContainer">
-                <div className="imgContainer">
-                    <NorthAmerica className="northAmerica"/>
-                    <Eurasia className="eurasia"/>
-                </div>
-            </div>
-            <div className="textContainer">
-                <h1>
-                    Text
+        <div className="international">
+            <div className="contentContainer">
+                <h1 className="internationalTitle">
+                    International
                 </h1>
+                <div className="mapContainer">
+                    <div className="imgContainer">
+                        <NorthAmerica className="northAmerica"/>
+                        <Eurasia className="eurasia"/>
+                    </div>
+                </div>
+                <div className="textContainer">
+                    <h1>
+                        Text
+                    </h1>
+                </div>
             </div>
         </div>
     );
