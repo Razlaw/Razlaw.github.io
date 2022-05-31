73ccad653ecfc5659af494d343827e691ef2736c
Razlaw <jan.razlaw@gmx.de>
2022-05-31 15:09:54 +0200

    Replace placeholder image on website project page

--- src/pages/weatherWebsite/WeatherWebsite.jsx
+++ src/pages/weatherWebsite/WeatherWebsite.jsx
@@ -2,7 +2,9 @@ import "./weatherWebsite.scss";
 
 import React from "react";
 
-import phoneImage from "../../assets/phone_downscaled.jpg";
+import phoneTemplateImage from "../../assets/template_phone_for_website_downscaled.png";
+import websiteImageOne from "../../assets/website_part1_downscaled.png";
+import websiteImageTwo from "../../assets/website_part2_downscaled.png";
 import {ReactComponent as ExternalLinkIcon} from '../../assets/icons/external_link.svg';
 
 export default function WeatherWebsite() {
@@ -10,7 +12,10 @@ export default function WeatherWebsite() {
         <div className="weatherWebsiteContainer">
             <h1 className="projectsHeadline">Meine Projekte</h1>
             <div className="imageContainer">
-                <img className="phoneImage" src={phoneImage} alt="Smartphone on table"/>
+                <img className="image websiteImageOne" src={websiteImageOne} alt="Smartphone on table"/>
+                <img className="image websiteImageTwo" src={websiteImageTwo} alt="Smartphone on table"/>
+                <img className="image phoneTemplateImage" src={phoneTemplateImage} alt="Smartphone on table"/>
+                <span className="swipeCue"></span>
             </div>
             <div className="textContainer">
                 <h1 className="headlineBig" id="projectName">Weather Website</h1>
