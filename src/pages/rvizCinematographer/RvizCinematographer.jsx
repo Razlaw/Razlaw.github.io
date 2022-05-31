23e1616f80d9743c57b764095ba91b305045103b
Razlaw <jan.razlaw@gmx.de>
2022-05-31 15:13:14 +0200

    Add react-intersection-observer to cinematographer page to start animation when page gets in view

--- src/pages/rvizCinematographer/RvizCinematographer.jsx
+++ src/pages/rvizCinematographer/RvizCinematographer.jsx
@@ -2,19 +2,25 @@ import "./rvizCinematographer.scss";
 
 import React from "react";
 
+import { useInView } from 'react-intersection-observer';
+
 import cameramanImage from "../../assets/cameraman_downscaled.jpg";
 import lineForCameramanImage from "../../assets/line_for_cameraman.png";
 import {ReactComponent as ExternalLinkIcon} from '../../assets/icons/external_link.svg';
 
 export default function RvizCinematographer() {
+    const { ref, inView } = useInView({
+        triggerOnce: true
+    });
+
     return (
         <div className="cinematographerContainer">
             <h1 className="projectsHeadline">Meine Projekte</h1>
-            <div className="imageContainer">
+            <div className={"imageContainer " + (inView && "inView")} ref={ref}>
                 <img className="cameramanImage" src={cameramanImage} alt="Cameraman on a hill with mountains in the background"/>
                 <img className="lineForCameramanImage" src={lineForCameramanImage} alt="Lines indicating motion of camera"/>
             </div>
-            <div className="textContainer">
+            <div className={"textContainer " + (inView && "inView")} ref={ref}>
                 <h1 className="headlineBig" id="projectName">RViz Cinematographer</h1>
                 <p className="text" id="projectDescription">
                     Komplexe Kamerafahrten<br/> benutzerfreundlich in RViz erstellen
