8919f470b6a0e9a16173954626e09360fbc938a5
Razlaw <jan.razlaw@gmx.de>
2022-06-02 16:30:21 +0200

    Add logo animation on start

--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,10 +1,24 @@
 import "./intro.scss";
+
+import { ReactComponent as Logo1000SVG } from "../../assets/icons/logo_scaled_1000.svg";
+import { ReactComponent as Logo4400SVG } from "../../assets/icons/logo_scaled_4400.svg";
+
 import portraitImage from "../../assets/portrait_crop.png";
 
 export default function Intro() {
+    const viewportDiagonal = Math.sqrt(window.innerHeight * window.innerHeight + window.innerWidth * window.innerWidth);
+
+    console.log("viewportDiagonal ", viewportDiagonal);
     return (
         <div className="intro">
             <div className="contentContainer">
+                <div className="logoContainer">
+                    {(
+                        viewportDiagonal < 1000 ?
+                        <Logo1000SVG className="logoSVG"/> :
+                        <Logo4400SVG className="logoSVG"/>
+                    )}
+                </div>
                 <div className="leftContainer">
                     <div className="imgContainer">
                         <img src={portraitImage} alt="Portrait of Jan Razlaw"/>
