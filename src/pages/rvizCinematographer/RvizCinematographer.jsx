b5417986ab6b78c8ff6bd53dc12fb5e25a895d04
Razlaw <jan.razlaw@gmx.de>
2022-06-11 23:21:04 +0200

    Use design of pose graph prediction page for rviz cinematographer page

--- src/pages/rvizCinematographer/RvizCinematographer.jsx
+++ src/pages/rvizCinematographer/RvizCinematographer.jsx
@@ -1,36 +1,57 @@
 import "./rvizCinematographer.scss";
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-import { useInView } from 'react-intersection-observer';
+import {useInView} from "react-intersection-observer";
 
 import cameramanImage from "../../assets/cameraman_downscaled.jpg";
 import lineForCameramanImage from "../../assets/line_for_cameraman.png";
-import {ReactComponent as ExternalLinkIcon} from '../../assets/icons/external_link.svg';
+import cameraOutlineImage from "../../assets/camera_outline_for_cameraman.png";
+
+import ExternalLinkButton from "../../components/externalLinkButton/ExternalLinkButton";
 
 export default function RvizCinematographer() {
     const { ref, inView } = useInView({
-        triggerOnce: true
+        triggerOnce: false
     });
+    const [wasInView, setWasInView] = useState(false);
+
+    useEffect(() => {
+        if(inView === true) {
+            setWasInView(true);
+        }
+    }, [inView]);
 
     return (
         <div className="cinematographerContainer">
-            <div className={"imageContainer " + (inView && "inView")} ref={ref}>
-                <img className="cameramanImage" src={cameramanImage} alt="Cameraman on a hill with mountains in the background"/>
-                <img className="lineForCameramanImage" src={lineForCameramanImage} alt="Lines indicating motion of camera"/>
+            <div className="imageFrame" ref={ref}>
+                <div className="imageContainer">
+                    <img
+                        className="cameramanImage"
+                        src={cameramanImage}
+                        alt="Cameraman on a hill with mountains in the background"
+                    />
+                    <img
+                        className={"cameraOutlineImage " + (inView && "inView")}
+                        src={cameraOutlineImage}
+                        alt="Outline around camera"
+                    />
+                    <img
+                        className={"lineForCameramanImage " + (inView && "inView")}
+                        src={lineForCameramanImage}
+                        alt="Lines indicating motion of camera"
+                    />
+                </div>
             </div>
-            <div className={"textContainer " + (inView && "inView")} ref={ref}>
-                <h1 className="headlineBig" id="projectName">RViz Cinematographer</h1>
-                <p className="text" id="projectDescription">
-                    Komplexe Kamerafahrten<br/> benutzerfreundlich in RViz erstellen
+            <div className={"textContainer " + (wasInView && "wasInView")}>
+                <h1>RViz Cinematographer</h1>
+                <p className="description">
+                    Komplexe Kamerafahrten<br/> benutzerfreundlich in RViz erstellen.
                 </p>
-                <a
-                    href="https://github.com/AIS-Bonn/rviz_cinematographer"
-                    target="_blank"
-                    rel="noreferrer">
-                    Code
-                    <ExternalLinkIcon/>
-                </a>
+                <ExternalLinkButton
+                    text={"Code"}
+                    link={"https://github.com/AIS-Bonn/rviz_cinematographer"}
+                />
             </div>
         </div>
     );
