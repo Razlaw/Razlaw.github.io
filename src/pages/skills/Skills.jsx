d32d99a27556a08af6e19807210ebd64a8cbfb56
Razlaw <jan.razlaw@gmx.de>
2022-06-13 18:30:45 +0200

    Update design of skills page

--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,45 +1,48 @@
 import "./skills.scss";
 
+import {useInView} from "react-intersection-observer";
+
 export default function Skills() {
+    const { ref, inView } = useInView({
+        triggerOnce: true
+    });
+
     return (
         <div className="skills">
             <div className="skillsContainer">
-                <h1 className="skillsTitle">
-                    Kenntnisse
+                <h1 className={"skillsTitle " + (inView && "inView")}>
+                    Bisherige Kenntnisse
                 </h1>
-                <div className="listsContainer">
-                    <div className="listContainer" id="listOne">
-                        <h1 className="listTitle">Sprachen</h1>
-                        <ul>
-                            <li><p className="text skill">C++</p></li>
-                            <li><p className="text skill">Python</p></li>
-                            <li><p className="text skill">JavaScript</p></li>
-                            <li><p className="text skill">HTML</p></li>
-                            <li><p className="text skill">CSS</p></li>
+                <div className="listsContainer" ref={ref}>
+                    <div className="listContainer">
+                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleOne">Sprachen</h1>
+                        <ul className={"skillList " + (inView && "inView")} id="listOne">
+                            <li><p className="description skill">C++</p></li>
+                            <li><p className="description skill">Python</p></li>
+                            <li><p className="description skill">JavaScript</p></li>
+                            <li><p className="description skill">HTML</p></li>
+                            <li><p className="description skill">CSS / Sass</p></li>
                         </ul>
                     </div>
-                    <div className="listContainer" id="listTwo">
-                        <h1 className="listTitle">List Title</h1>
-                        <ul>
-                            <li><p className="text skill">List Item one</p></li>
-                            <li><p className="text skill">List Item two</p></li>
-                            <li><p className="text skill">Longer List Item one</p></li>
+                    <div className="listContainer">
+                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleTwo">Bibliotheken</h1>
+                        <ul className={"skillList " + (inView && "inView")} id="listTwo">
+                            <li><p className="description skill">Point Cloud Library</p></li>
+                            <li><p className="description skill">OpenCV</p></li>
+                            <li><p className="description skill">PyTorch</p></li>
+                            <li><p className="description skill">PyTorch Geometric</p></li>
+                            <li><p className="description skill">React</p></li>
                         </ul>
                     </div>
-                    <div className="listContainer" id="listThree">
-                        <h1 className="listTitle">List Title</h1>
-                        <ul>
-                            <li><p className="text skill">List Item one</p></li>
-                            <li><p className="text skill">List Item two</p></li>
-                            <li><p className="text skill">Longer List Item one</p></li>
-                        </ul>
-                    </div>
-                    <div className="listContainer" id="listFour">
-                        <h1 className="listTitle">List Title</h1>
-                        <ul>
-                            <li><p className="text skill">List Item one</p></li>
-                            <li><p className="text skill">List Item two</p></li>
-                            <li><p className="text skill">Longer List Item one</p></li>
+                    <div className="listContainer">
+                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleThree">Sonstiges</h1>
+                        <ul className={"skillList " + (inView && "inView")} id="listThree">
+                            <li><p className="description skill">Linux</p></li>
+                            <li><p className="description skill">Git</p></li>
+                            <li><p className="description skill">ROS</p></li>
+                            <li><p className="description skill">Qt Creator</p></li>
+                            <li><p className="description skill">LaTex</p></li>
+                            <li><p className="description skill">Inkscape</p></li>
                         </ul>
                     </div>
                 </div>
