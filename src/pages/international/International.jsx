84363f0ecaeb8ca2cb89e08696d3e22960e0f01b
Razlaw <jan.razlaw@gmx.de>
2022-06-27 16:20:04 +0200

    Add links from international page to projects descriptions page and paper website

--- src/pages/international/International.jsx
+++ src/pages/international/International.jsx
@@ -2,6 +2,8 @@ import "./international.scss";
 
 import {useInView} from "react-intersection-observer";
 
+import {useNavigate} from "react-router-dom";
+
 import {ReactComponent as NorthAmerica} from '../../assets/north_america_wo_borders.svg';
 import {ReactComponent as Eurasia} from '../../assets/eurasia_wo_borders.svg';
 
@@ -14,6 +16,8 @@ export default function International() {
     const { ref, inView } = useInView({
         triggerOnce: true
     });
+
+    const navigate = useNavigate();
 
     return (
         <div className="international">
@@ -28,28 +32,44 @@ export default function International() {
                     </div>
                 </div>
                 <div className="textContainer" ref={ref}>
-                    <div className={"locationBox " + (inView && "inView")} id="losAngeles">
+                    <div
+                        className={"locationBox " + (inView && "inView")}
+                        id="losAngeles"
+                        onClick={() => {navigate("/projects_descriptions/openid=4")}}
+                    >
                         <h1 className="internationalEventTitle">2015</h1>
                         <h1 className="internationalEventTitle">Los Angeles</h1>
                         <FourthPlaceMedal className="medal"/>
                         <h2 className="internationalEventDescription">Robotik Wettbewerb</h2>
                         <h2 className="internationalEventDescription">DRC</h2>
                     </div>
-                    <div className={"locationBox " + (inView && "inView")} id="abuDhabi1">
+                    <div
+                        className={"locationBox " + (inView && "inView")}
+                        id="abuDhabi1"
+                        onClick={() => {navigate("/projects_descriptions/openid=6")}}
+                    >
                         <h1 className="internationalEventTitle">2017</h1>
                         <h1 className="internationalEventTitle">Abu Dhabi</h1>
                         <GoldMedal className="medal"/>
                         <h2 className="internationalEventDescription">Robotik Wettbewerb</h2>
                         <h2 className="internationalEventDescription">MRZIRC</h2>
                     </div>
-                    <div className={"locationBox " + (inView && "inView")} id="montreal">
-                        <h1 className="internationalEventTitle">2019</h1>
-                        <h1 className="internationalEventTitle">Montreal</h1>
-                        <ScientificPaper className="paperIcon"/>
-                        <h2 className="internationalEventDescription">Konferenz</h2>
-                        <h2 className="internationalEventDescription">ICRA</h2>
+                    <div
+                        className={"locationBox " + (inView && "inView")}
+                        id="montreal"
+                        onClick={() => {window.open("https://scholar.google.com/citations?view_op=view_citation&hl=de&user=bcx5-kkAAAAJ&citation_for_view=bcx5-kkAAAAJ:qjMakFHDy7sC", "_blank").focus()}}
+                    >
+                            <h1 className="internationalEventTitle">2019</h1>
+                            <h1 className="internationalEventTitle">Montreal</h1>
+                            <ScientificPaper className="paperIcon"/>
+                            <h2 className="internationalEventDescription">Konferenz</h2>
+                            <h2 className="internationalEventDescription">ICRA</h2>
                     </div>
-                    <div className={"locationBox " + (inView && "inView")} id="abuDhabi2">
+                    <div
+                        className={"locationBox " + (inView && "inView")}
+                        id="abuDhabi2"
+                        onClick={() => {navigate("/projects_descriptions/openid=7")}}
+                    >
                         <h1 className="internationalEventTitle">2020</h1>
                         <h1 className="internationalEventTitle">Abu Dhabi</h1>
                         <SilverMedal className="medal"/>
