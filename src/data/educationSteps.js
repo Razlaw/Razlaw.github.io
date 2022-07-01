2e8c713d11ada52484954221cee896d4c7c048f5
Razlaw <jan.razlaw@gmx.de>
2022-07-01 20:27:07 +0200

    Refactoring - Extract data from Education.jsx and use map to iterate over data

---
+++ src/data/educationSteps.js
@@ -0,0 +1,32 @@
+export const education_steps = [
+    {
+        title: "Autodidaktische Weiterbildung",
+        subtitle: "Front-End-Webentwicklung",
+        date: "Nov. 2021 - Jul. 2022"
+    },
+    {
+        title: "Promotions\xadstipendium",
+        subtitle: "Verfolgung und Vorhersage mittels Graph Neuronaler Netze",
+        date: "Nov. 2018 - Okt. 2021"
+    },
+    {
+        title: "Master of Science - Computer Science - 1,3",
+        subtitle: "Abschlussarbeit: \"Detection and Tracking of Small Objects in Sparse 3D Laser Range Data\" - 1,1",
+        date: "Apr. 2015 - Okt. 2018"
+    },
+    {
+        title: "Bachelor of Science - Informatik - 1,7",
+        subtitle: "Abschlussarbeit: \"Evaluation von 3D-Registrierverfahren auf spärlichen 3D-Laserscans\" - 1,0",
+        date: "Okt. 2011 - Jan. 2015"
+    },
+    {
+        title: "Zivildienst",
+        subtitle: "",
+        date: "Nov. 2010 - Apr. 2011"
+    },
+    {
+        title: "Allgemeine Hochschulreife - 1,8",
+        subtitle: "Leistungskurse: Mathematik & Physik",
+        date: "Aug. 2008 - Jun. 2010"
+    }
+];
--- src/pages/education/Education.jsx
+++ src/pages/education/Education.jsx
@@ -4,43 +4,24 @@ import React from "react";
 
 import Logo from "../../components/logo/Logo";
 
+import {education_steps} from "../../data/educationSteps";
+
+
 export default function Education() {
     return (
         <div className="educationContainer">
             <Logo/>
             <div className="contentContainer">
                 <h1 className="educationTitle">Bildungsweg</h1>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Autodidaktische Weiterbildung</h1>
-                    <h2 className="educationStepSubTitle">Front-End-Webentwicklung</h2>
-                    <p className="educationStepDate">Nov. 2021 - Jul. 2022</p>
-                </div>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Promotions&shy;stipendium</h1>
-                    <h2 className="educationStepSubTitle">Verfolgung und Vorhersage mittels Graph Neuronaler Netze</h2>
-                    <p className="educationStepDate">Nov. 2018 - Okt. 2021</p>
-                </div>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Master of Science - Computer Science - 1,3</h1>
-                    <h2 className="educationStepSubTitle">Abschlussarbeit: "Detection and Tracking of Small Objects in
-                        Sparse 3D Laser Range Data" - 1,1</h2>
-                    <p className="educationStepDate">Apr. 2015 - Okt. 2018</p>
-                </div>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Bachelor of Science - Informatik - 1,7</h1>
-                    <h2 className="educationStepSubTitle">Abschlussarbeit: "Evaluation von 3D-Registrierverfahren auf
-                        spärlichen 3D-Laserscans" - 1,0</h2>
-                    <p className="educationStepDate">Okt. 2011 - Jan. 2015</p>
-                </div>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Zivildienst</h1>
-                    <p className="educationStepDate">Nov. 2010 - Apr. 2011</p>
-                </div>
-                <div className="educationStepContainer">
-                    <h1 className="educationStepTitle">Allgemeine Hochschulreife - 1,8</h1>
-                    <h2 className="educationStepSubTitle">Leistungskurse: Mathematik & Physik</h2>
-                    <p className="educationStepDate">Aug. 2008 - Jun. 2010</p>
-                </div>
+                {education_steps.map((education_step, key) => {
+                    return (
+                        <div className="educationStepContainer" key={"educationStep" + key}>
+                            <h1 className="educationStepTitle">{education_step.title}</h1>
+                            <h2 className="educationStepSubTitle">{education_step.subtitle}</h2>
+                            <p className="educationStepDate">{education_step.date}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
