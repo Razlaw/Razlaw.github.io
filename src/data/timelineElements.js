954d695405e660960079ca9489be4b06fd49587e
Razlaw <jan.razlaw@gmx.de>
2022-07-01 20:28:05 +0200

    Add draft for timeline page

---
+++ src/data/timelineElements.js
@@ -0,0 +1,136 @@
+export const timeline_data = [
+    {
+        education_title: "Weiterbildung",
+        timeline_elements:
+            [
+                {
+                    icon_id: "coding",
+                    date: "Mai. 22 - Jul. 22",
+                    title: "Portfolio Webseite",
+                    link_text: "",
+                    link_destination: ""
+                },
+                {
+                    icon_id: "coding",
+                    date: "Dez. 21 - Mär. 22",
+                    title: "Wetter Webseite",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=11"
+                }
+            ]
+    },
+    {
+        education_title: "Promotion",
+        timeline_elements:
+            [
+                {
+                    icon_id: "work",
+                    date: "Sep. 20 - Okt. 21",
+                    title: "Vorhersage von Personenposen mittels Graph Neuronaler Netze",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=9"
+                },
+                {
+                    icon_id: "work",
+                    date: "Jan. 22 - Nov. 20",
+                    title: "Detektion und Tracking von Personen in Lasermessungen",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=8"
+                },
+                {
+                    icon_id: "work",
+                    date: "Jan. 22 - Jul. 22",
+                    title: "Multi Objekt Tracking mit Graph Neuronalen Netzen",
+                    link_text: "",
+                    link_destination: ""
+                },
+                {
+                    icon_id: "work",
+                    date: "Jan. 22 - Jul. 22",
+                    title: "Portfolio Webseite",
+                    link_text: "Beschreibung",
+                    link_destination: ""
+                }
+            ]
+    },
+    {
+        education_title: "Master",
+        timeline_elements:
+            [
+                {
+                    icon_id: "work",
+                    date: "Sep. 20 - Okt. 21",
+                    title: "Vorhersage von Personenposen mittels Graph Neuronaler Netze",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=9"
+                },
+                {
+                    icon_id: "work",
+                    date: "Jan. 22 - Nov. 20",
+                    title: "Detektion und Tracking von Personen in Lasermessungen",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=8"
+                }
+            ]
+    },
+    {
+        education_title: "Bachelor",
+        timeline_elements:
+            [
+                {
+                    icon_id: "work",
+                    date: "Sep. 20 - Okt. 21",
+                    title: "Vorhersage von Personenposen mittels Graph Neuronaler Netze",
+                    link_text: "Beschreibung",
+                    link_destination: "https://razlaw.github.io/#/projects_descriptions/openid=9"
+                },
+                {
+                    icon_id: "work",
+                    date: "2012 - Aug. 11",
+                    title: "Ferienaushilfe im Zentrallager der Bruno Kleine GmbH",
+                    link_text: "",
+                    link_destination: ""
+                }
+            ]
+    },
+    {
+        education_title: "Zivildienst",
+        timeline_elements:
+            [
+                {
+                    icon_id: "work",
+                    date: "Nov. 10 - Aug. 11",
+                    title: "Aushilfe im Zentrallager der Bruno Kleine GmbH",
+                    link_text: "Webseite Bruno Kleine GmbH",
+                    link_destination: "https://www.bruno-kleine.de/"
+                }
+            ]
+    },
+    {
+        education_title: "Gymnasium",
+        timeline_elements:
+            [
+                {
+                    icon_id: "work",
+                    date: "2008 - 2010",
+                    title: "Aushilfe im Zentrallager der Bruno Kleine GmbH",
+                    link_text: "",
+                    link_destination: ""
+                },
+                {
+                    icon_id: "work",
+                    date: "2007 - 2008",
+                    title: "Aushilfe als Hausmeistergehilfe bei der Bruno Kleine GmbH",
+                    link_text: "",
+                    link_destination: ""
+                },
+                {
+                    icon_id: "work",
+                    date: "2005 - 2007",
+                    title: "Zeitungsausträger",
+                    link_text: "",
+                    link_destination: ""
+                }
+            ]
+    }
+];
--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,16 +1,77 @@
 import "./timeline.scss";
+import 'react-vertical-timeline-component/style.min.css';
 
 import React from "react";
 
+import {useNavigate} from "react-router-dom";
+
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
+
 import Logo from "../../components/logo/Logo";
 
+import {ReactComponent as WorkIcon} from '../../assets/icons/work.svg';
+import {ReactComponent as CodingIcon} from '../../assets/icons/coding.svg';
+
+import {timeline_data} from "../../data/timelineElements";
+
+function getIcon(icon_id) {
+    switch (icon_id) {
+        case "coding":
+            return <CodingIcon/>;
+        case "work":
+            return <WorkIcon/>;
+        default:
+            return <WorkIcon/>;
+    }
+}
+
 export default function Timeline() {
+    const navigate = useNavigate();
+
     return (
         <div className="timelineContainer">
             <Logo/>
+            <h1 className="timelineTitle">Zeitstrahl</h1>
             <div className="contentContainer">
-                <h1>Zeitstrahl</h1>
-                <h2>folgt...</h2>
+                {timeline_data.map((data, key) => {
+                    return (
+                        <div className="timelinePartContainer" key={key}>
+                            <div
+                                className="educationTimelineContainer"
+                                onClick={() => {navigate("/education")}}
+                            >
+                                <div className="educationTimelineContent" id={"educationTitle" + key}>
+                                    <p>
+                                        {data.education_title}
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="workContainer">
+                                <VerticalTimeline layout='1-column-left'>
+                                    {data.timeline_elements.map((element_data, element_key) => {
+                                        return (
+                                            <VerticalTimelineElement
+                                                date={element_data.date}
+                                                icon={getIcon(element_data.icon_id)}
+                                                key={element_key}
+                                            >
+                                                <h1 className="vertical-timeline-element-title">{element_data.title}</h1>
+                                                <a
+                                                    className="vertical-timeline-element-subtitle"
+                                                    href={element_data.link_destination}
+                                                    target="_blank"
+                                                    rel="noreferrer"
+                                                >
+                                                    {element_data.link_text}
+                                                </a>
+                                            </VerticalTimelineElement>
+                                        )
+                                    })}
+                                </VerticalTimeline>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
