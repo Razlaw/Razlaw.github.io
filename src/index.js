9307240ea941248436d3d48a85df1bf8c55b5d66
Razlaw <jan.razlaw@gmx.de>
2022-07-02 17:34:58 +0200

    Selfhost fonts part 2 - Lato font was imported by semantic-ui-css
    Removed dependecies on semantic-ui-css and -react, keeping an adapted version of the used css file from the first package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import 'semantic-ui-css/semantic.min.css';
+import "./semantic-ui-min-wo-google.css";
 import "@fontsource/playfair-display/700.css";
 import "@fontsource/roboto-slab/700.css"
 import "@fontsource/roboto-slab/400.css"
 import "@fontsource/pt-serif";
+import "@fontsource/lato/700.css"
+import "@fontsource/lato/400.css"
 
 import React from 'react';
 import ReactDOM from 'react-dom';
