23ebb070371dc97fdcdec1229e113da3516e4bec
Razlaw <jan.razlaw@gmx.de>
2022-07-02 13:49:58 +0200

    Selfhost fonts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import 'semantic-ui-css/semantic.min.css';
+import "@fontsource/playfair-display/700.css";
+import "@fontsource/roboto-slab/700.css"
+import "@fontsource/roboto-slab/400.css"
+import "@fontsource/pt-serif";
 
 import React from 'react';
 import ReactDOM from 'react-dom';
