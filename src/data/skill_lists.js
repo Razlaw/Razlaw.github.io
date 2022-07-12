116a32e1487cfa66f4d140c176822d4375c5f08d
Razlaw <jan.razlaw@gmx.de>
2022-07-12 19:21:35 +0200

    Refactoring - Extract data from skills page

---
+++ src/data/skill_lists.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const skill_lists = [
+    {
+        title: "Sprachen",
+        skills: [
+            "C++",
+            "Python",
+            "JavaScript",
+            "HTML",
+            "CSS / Sass"
+        ]
+    },
+    {
+        title: "Bibliotheken",
+        skills: [
+            "Point Cloud Library",
+            "OpenCV",
+            "PyTorch",
+            "PyTorch Geometric",
+            "React"
+        ]
+    },
+    {
+        title: "Sonstiges",
+        skills: [
+            "Linux",
+            "Git",
+            "ROS",
+            "Qt Creator",
+            "LaTex",
+            "Inkscape"
+        ]
+    }
+];
--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,6 +1,10 @@
 import "./skills.scss";
 
+import React from "react";
+
 import {useInView} from "react-intersection-observer";
+
+import {skill_lists} from "../../data/skill_lists";
 
 export default function Skills() {
     const { ref, inView } = useInView({
@@ -14,37 +18,20 @@ export default function Skills() {
                     Bisherige Kenntnisse
                 </h1>
                 <div className="listsContainer" ref={ref}>
-                    <div className="listContainer">
-                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleOne">Sprachen</h1>
-                        <ul className={"skillList " + (inView && "inView")} id="listOne">
-                            <li><p className="description skill">C++</p></li>
-                            <li><p className="description skill">Python</p></li>
-                            <li><p className="description skill">JavaScript</p></li>
-                            <li><p className="description skill">HTML</p></li>
-                            <li><p className="description skill">CSS / Sass</p></li>
-                        </ul>
-                    </div>
-                    <div className="listContainer">
-                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleTwo">Bibliotheken</h1>
-                        <ul className={"skillList " + (inView && "inView")} id="listTwo">
-                            <li><p className="description skill">Point Cloud Library</p></li>
-                            <li><p className="description skill">OpenCV</p></li>
-                            <li><p className="description skill">PyTorch</p></li>
-                            <li><p className="description skill">PyTorch Geometric</p></li>
-                            <li><p className="description skill">React</p></li>
-                        </ul>
-                    </div>
-                    <div className="listContainer">
-                        <h1 className={"listTitle " + (inView && "inView")} id="listTitleThree">Sonstiges</h1>
-                        <ul className={"skillList " + (inView && "inView")} id="listThree">
-                            <li><p className="description skill">Linux</p></li>
-                            <li><p className="description skill">Git</p></li>
-                            <li><p className="description skill">ROS</p></li>
-                            <li><p className="description skill">Qt Creator</p></li>
-                            <li><p className="description skill">LaTex</p></li>
-                            <li><p className="description skill">Inkscape</p></li>
-                        </ul>
-                    </div>
+                    {skill_lists.map((skillList, key) => {
+                        return (
+                            <div className="listContainer" key={key}>
+                                <h1 className={"listTitle " + (inView && "inView")} id={"skillListTitle" + key}>{skillList.title}</h1>
+                                <ul className={"skillList " + (inView && "inView")} id={"skillList" + key}>
+                                    {skillList.skills.map((skill, skillKey) => {
+                                        return (
+                                            <li key={skillKey}><p className="description skill">{skill}</p></li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
